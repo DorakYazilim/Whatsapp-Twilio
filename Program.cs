26c3e2e0573fcab92afebbc0a0573df39bb96af2
Mustafa DILMEN <mustafa.dilmen@dorakholding.com.tr>
2019-09-26 18:01:49 +0300

    Initial

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
@@ -7,20 +9,53 @@ namespace WhatsApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             TwilioClient.Init(
-                Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID"),
-                Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN")
+                "AC2940e764b1b882be5333eb41c3cdcdae",
+                "6262a00a83696c0cac2dfc12e7582163"
             );
 
-            var message = MessageResource.Create(
-                from: new PhoneNumber("whatsapp:TWILIO_WHATSAPP_NUMBER"),
-                to: new PhoneNumber("whatsapp:YOUR_MOBILE_NUMBER"),
-                body: "Ahoy from Twilio!"
-            );
+            List<Uri> uriList = new List<Uri>();
 
-            Console.WriteLine("Message SID: " + message.Sid);
+            Uri newUri = new Uri("https://demo.twilio.com/owl.png");
+
+            uriList.Add(newUri);
+
+            for (int i = 0; i < 20; i++)
+            {
+                await SendMessageASync(uriList, i);
+            }
+            
+
+            //var message3 = MessageResource.Create(
+            //    from: new PhoneNumber("whatsapp:+14155238886"),
+            //    to: new PhoneNumber("whatsapp:+905343971007"),
+            //    body: "Ahoy Serdar! Ahoy Ahoy",
+            //    mediaUrl: uriList
+            //);
+
+
+            // Console.WriteLine("Message SID: " + message3.Sid);
+        }
+        public static async Task<bool> SendMessageASync(List<Uri> uriList, int count)
+        {
+            try
+            {
+                var message = await MessageResource.CreateAsync(
+                    from: new PhoneNumber("whatsapp:+14155238886"),
+                    to: new PhoneNumber("whatsapp:+905383977377"),
+                    body: "Ahoy from A-101! Ahoy Ahoy " + count
+                // mediaUrl: uriList
+                );
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
         }
     }
 }
